c6a5716d8e1a8b3353c867bb7efff7441a56de09
teja <teja@gmail.com>
2024-03-20 10:49:04 +0530

    Initial Project

---
+++ GitHubProject/src/main/java/com/nt/GitHubProjectApplication.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHubProjectApplication.class, args);
+	}
+
+}
